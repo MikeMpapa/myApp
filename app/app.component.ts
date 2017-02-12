824b93ba6ea3f61f7e6d89078d6132470867f019
MikeMpapa <michalis.papakostas@mavs.uta.edu>
2017-02-12 13:36:31 +0200

    added users menu

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,12 +3,68 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="jumbotron">
-      <h1>Welcome to Our App!</h1>
-    </div>
+      <header>
+          <nav class="navbar navbar-inverse" >
+              <div class="navbar navbar-header">
+              <a href="/" class="navbar-brand">My Angular 2 App!</a>
+              </div>
+          </nav>
+      </header>
+
+      <main>
+
+          <div class="row">
+
+              <div class="col-sm-4">
+
+
+               <div *ngIf="users">
+                  <ul class="list-group users-list" >
+                     <li class="list-group-item"
+                         *ngFor="let user of users">
+                          {{user.name}}, ({{user.username}})
+                     </li>
+                  </ul>
+              </div>
+
+              </div>
+              <div class="col-sm-8">
+
+                  <div class="jumbotron">
+                      <h1>Welcome to Our App!</h1>
+                      <p>{{message}}</p>
+                  </div>
+
+              </div>
+          </div>
+
+          
+
+         
+
+      </main>
+
+
+
+
+      <footer class="text-center">
+          Copyright &copy; 2017
+      </footer>
+
+
   `,
   styles: [`
     .jumbotron { box-shadow: 0 2px 0 rgba(0, 0, 0, 0.2); }
   `]
 })
-export class AppComponent {}
+
+
+export class AppComponent {
+    message = "Hello";
+    users = [
+    {id:1, name:'Michalis', username:'Papakostas'},
+    {id:2, name:'Olga', username:'Alexaki'},
+    {id:3, name:'Paris', username:'Christogeorgos'},
+    {id:4, name:'Annoula', username:'Arguraki'}
+    ];
+}
